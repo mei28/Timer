fa8d7381e0e2d4621afced71141b9399b413ede6
MingZhe Yang <ming.aist@gmail.com>
2019-09-11 23:00:37 +0900

    タイマー機能の実装

--- Assets/SettingScene/SettingManager.cs
+++ Assets/SettingScene/SettingManager.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public class SettingManager : MonoBehaviour
-{
+public class SettingManager : MonoBehaviour {
+  /// <summary>
+  /// タイマーの状態
+  /// </summary>
+  private bool isRunning = false;
   /// <summary>
   /// タイマーを変更させるもの
   /// </summary>
@@ -17,30 +21,58 @@ public class SettingManager : MonoBehaviour
   /// </summary>
   private int maxTime = 5999;
   // Start is called before the first frame update
-  void Start()
-  {
+  void Start () {
     restTime = 0;
-    UpDateTime(restTime);
+    uiManager = GetComponent<UIManager> ();
+    UpDateTime ();
   }
 
   // Update is called once per frame
-  void Update()
-  {
+  void Update () {
+    if (isRunning) {
+      restTime -= Time.deltaTime;
 
+      if (restTime < 0) {
+        Debug.Log ("Finish!");
+        restTime = 0;
+        isRunning = false;
+
+      }
+      UpDateTime ();
+    }
   }
 
-  void UpDateTime(float restTime)
-  {
-    uiManager.UpDateTimeText(restTime);
+  void UpDateTime () {
+    uiManager.UpDateTimeText (restTime);
   }
   /// <summary>
   /// ボタンを押したときに時間をどうするか
   /// </summary>
   /// <param name="value">追加する時間</param>
-  public void AddTime(int value)
-  {
+  public void AddTime (int value) {
     restTime += value;
-    restTime = Mathf.Clamp(restTime, 0, maxTime);
-    UpDateTime(restTime);
+    restTime = Mathf.Clamp (restTime, 0, maxTime);
+    UpDateTime ();
+  }
+  /// <summary>
+  /// スタートボタンを押したとき
+  /// </summary>
+  public void OnStartButton () {
+    isRunning = true;
+    uiManager.StartTimer ();
+  }
+  /// <summary>
+  /// ストップボタンを押したとき
+  /// </summary>
+  public void OnStopButton () {
+    isRunning = false;
+    uiManager.StopTimer ();
+  }
+
+  public void OnButton () {
+    if (isRunning)
+      OnStopButton ();
+    else
+      OnStartButton ();
   }
 }
--- Assets/SettingScene/UIManager.cs
+++ Assets/SettingScene/UIManager.cs
@@ -1,18 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-
-public class UIManager : MonoBehaviour
-{
+public class UIManager : MonoBehaviour {
   /// <summary>
   /// 設定用のタイマー表示
   /// </summary>
-  [SerializeField] private Text timerText;
+  [SerializeField] private TextMeshProUGUI timerText;
   /// <summary>
   /// スタートボタン
   /// </summary>
-  [SerializeField] private Text StartBotton;
+  [SerializeField] private TextMeshProUGUI StartBotton;
   /// <summary>
   /// 設定用のボタン群
   /// </summary>
@@ -23,8 +22,15 @@ public class UIManager : MonoBehaviour
   /// タイマーのテキストの更新
   /// </summary>
   /// <param name="time">変更する時間</param>
-  public void UpDateTimeText(float time)
-  {
-    timerText.text = string.Format("{0:00}:{1:00}", (int)(time / 60), time % 60);
+  public void UpDateTimeText (float time) {
+    timerText.text = string.Format ("{0:00}:{1:00}", (int) (time / 60), time % 60);
+  }
+
+  public void StartTimer () {
+    StartBotton.text = "STOP";
+
+  }
+  public void StopTimer () {
+    StartBotton.text = "START";
   }
 }
